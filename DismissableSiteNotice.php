926e64a705be7a30ad19cfe279b4a0e5dd7d31c1
FFS <FreedomFighterSparrow@kolzchut.org.il>
2014-02-12 23:09:01 +0200

    Allow anonymous users to dismiss the sitenotice
    Sometime in the past, anonymous users lost the ability to dismiss the
    sitenotice; this brings it back.
    For backwards compatibility, it needs to be enabled using
    $wgDismissableSiteNoticeForAnon, which is FALSE by default.
    
    Bug: 57732
    Change-Id: I87df3301c45fabf62d06376cbba433ef2cc6c355

--- DismissableSiteNotice.php
+++ DismissableSiteNotice.php
@@ -1,5 +1,23 @@
 <?php
-if ( !defined( 'MEDIAWIKI' ) ) die();
+/**
+ * DismissableSiteNotice extension - allows users to dismiss (hide)
+ * the sitenotice.
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.1
+ * @author Brion Vibber
+ * @author Kevin Israel
+ * @author Dror S.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @link http://www.mediawiki.org/wiki/Extension:DismissableSiteNotice Documentation
+ */
+
+ # Not a valid entry point, skip unless MEDIAWIKI is defined
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "This is a MediaWiki extension";
+	exit( 1 );
+}
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
@@ -7,7 +25,9 @@ $wgExtensionCredits['other'][] = array(
 	'author' => array(
 		'Brion Vibber',
 		'Kevin Israel',
+		'Dror S.',
 	),
+	'version' => '1.0.1',
 	'descriptionmsg' => 'sitenotice-desc',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:DismissableSiteNotice',
 );
@@ -34,41 +54,43 @@ $wgResourceModules['ext.dismissableSiteNotice'] = array(
  * @return bool true
  */
 $wgHooks['SiteNoticeAfter'][] = function( &$notice, $skin ) {
-	global $wgMajorSiteNoticeID;
+	global $wgMajorSiteNoticeID, $wgDismissableSiteNoticeForAnons;
 
 	if ( !$notice ) {
 		return true;
 	}
 
-	// No dismissal for anons
+	// Dismissal for anons is configurable
+	if ( $wgDismissableSiteNoticeForAnons || $skin->getUser()->isLoggedIn() ) {
+		// Cookie value consists of two parts
+		$major = (int) $wgMajorSiteNoticeID;
+		$minor = (int) $skin->msg( 'sitenotice_id' )->inContentLanguage()->text();
+
+		$out = $skin->getOutput();
+		$out->addModules( 'ext.dismissableSiteNotice' );
+		$out->addJsConfigVars( 'wgSiteNoticeId', "$major.$minor" );
+
+		$notice = Html::rawElement( 'div', array( 'class' => 'mw-dismissable-notice' ),
+			Html::rawElement( 'div', array( 'class' => 'mw-dismissable-notice-close' ),
+				$skin->msg( 'sitenotice_close-brackets' )
+				->rawParams(
+					Html::element( 'a', array( 'href' => '#' ), $skin->msg( 'sitenotice_close' )->text() )
+				)
+				->escaped()
+			) .
+			Html::rawElement( 'div', array( 'class' => 'mw-dismissable-notice-body' ), $notice )
+		);
+	}
+
 	if ( $skin->getUser()->isAnon() ) {
 		// Hide the sitenotice from search engines (see bug 9209 comment 4)
 		// XXX: Does this actually work?
 		$notice = Html::inlineScript( Xml::encodeJsCall( 'document.write', array( $notice ) ) );
-		return true;
 	}
-
-	// Cookie value consists of two parts
-	$major = (int) $wgMajorSiteNoticeID;
-	$minor = (int) $skin->msg( 'sitenotice_id' )->inContentLanguage()->text();
-
-	$out = $skin->getOutput();
-	$out->addModules( 'ext.dismissableSiteNotice' );
-	$out->addJsConfigVars( 'wgSiteNoticeId', "$major.$minor" );
-
-	$notice = Html::rawElement( 'div', array( 'class' => 'mw-dismissable-notice' ),
-		Html::rawElement( 'div', array( 'class' => 'mw-dismissable-notice-close' ),
-			$skin->msg( 'sitenotice_close-brackets' )
-			->rawParams(
-				Html::element( 'a', array( 'href' => '#' ), $skin->msg( 'sitenotice_close' )->text() )
-			)
-			->escaped()
-		) .
-		Html::rawElement( 'div', array( 'class' => 'mw-dismissable-notice-body' ), $notice )
-	);
 
 	return true;
 };
 
 // Default settings
 $wgMajorSiteNoticeID = 1;
+$wgDismissableSiteNoticeForAnons = false;
