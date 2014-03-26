6596063cf40d643c816c5b9bd1298017e6c44d65
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-26 15:17:52 +0100

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: I0c94a97e6963ddf8f80fff6cb52918daea6ea402

--- DismissableSiteNotice.php
+++ DismissableSiteNotice.php
@@ -12,6 +12,7 @@ $wgExtensionCredits['other'][] = array(
 	'url' => 'https://www.mediawiki.org/wiki/Extension:DismissableSiteNotice',
 );
 
+$wgMessagesDirs['DismissableSiteNotice'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['DismissableSiteNotice'] = __DIR__ . '/DismissableSiteNotice.i18n.php';
 
 $wgResourceModules['ext.dismissableSiteNotice'] = array(
