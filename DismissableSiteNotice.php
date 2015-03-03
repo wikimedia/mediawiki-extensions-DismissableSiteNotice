c00fc267437249dfa4f879b21cc9ff6b83fbcf6b
Kunal Mehta <legoktm@gmail.com>
2015-03-02 19:51:24 -0800

    Move SiteNoticeAfter hook to separate file
    Change-Id: If1a928e522ac9cb420d33d83070255bc74922cf4

--- DismissableSiteNotice.php
+++ DismissableSiteNotice.php
@@ -33,6 +33,7 @@ $wgExtensionCredits['other'][] = array(
 );
 
 $wgMessagesDirs['DismissableSiteNotice'] = __DIR__ . '/i18n';
+$wgAutoloadClasses['DismissableSiteNoticeHooks'] = __DIR__ . '/DismissableSiteNotice.hooks.php';
 
 $wgResourceModules['ext.dismissableSiteNotice'] = array(
 	'localBasePath' => __DIR__ . '/modules',
@@ -47,48 +48,7 @@ $wgResourceModules['ext.dismissableSiteNotice'] = array(
 	'position' => 'top',
 );
 
-/**
- * @param string $notice
- * @param Skin $skin
- * @return bool true
- */
-$wgHooks['SiteNoticeAfter'][] = function( &$notice, $skin ) {
-	global $wgMajorSiteNoticeID, $wgDismissableSiteNoticeForAnons;
-
-	if ( !$notice ) {
-		return true;
-	}
-
-	// Dismissal for anons is configurable
-	if ( $wgDismissableSiteNoticeForAnons || $skin->getUser()->isLoggedIn() ) {
-		// Cookie value consists of two parts
-		$major = (int) $wgMajorSiteNoticeID;
-		$minor = (int) $skin->msg( 'sitenotice_id' )->inContentLanguage()->text();
-
-		$out = $skin->getOutput();
-		$out->addModules( 'ext.dismissableSiteNotice' );
-		$out->addJsConfigVars( 'wgSiteNoticeId', "$major.$minor" );
-
-		$notice = Html::rawElement( 'div', array( 'class' => 'mw-dismissable-notice' ),
-			Html::rawElement( 'div', array( 'class' => 'mw-dismissable-notice-close' ),
-				$skin->msg( 'sitenotice_close-brackets' )
-				->rawParams(
-					Html::element( 'a', array( 'href' => '#' ), $skin->msg( 'sitenotice_close' )->text() )
-				)
-				->escaped()
-			) .
-			Html::rawElement( 'div', array( 'class' => 'mw-dismissable-notice-body' ), $notice )
-		);
-	}
-
-	if ( $skin->getUser()->isAnon() ) {
-		// Hide the sitenotice from search engines (see bug 9209 comment 4)
-		// XXX: Does this actually work?
-		$notice = Html::inlineScript( Xml::encodeJsCall( 'document.write', array( $notice ) ) );
-	}
-
-	return true;
-};
+$wgHooks['SiteNoticeAfter'][] = 'DismissableSiteNoticeHooks::onSiteNoticeAfter';
 
 // Default settings
 $wgMajorSiteNoticeID = 1;
