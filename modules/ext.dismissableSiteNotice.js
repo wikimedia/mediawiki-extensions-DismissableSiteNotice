4479c9f309c0baec44b1955038fdab76968bbfa4
Kaldari <rkaldari@wikimedia.org>
2015-10-28 15:58:41 -0600

    Make dismissable SiteNotices hidden by default so they don't flash
    Note this does not affect the accessibility of the SiteNotices.
    
    Besides T108513, this also fixes the dismiss button jumping from the
    top of the notice to the side during page load.
    
    Also, updating a bug number in a comment to the Phabricator ID.
    
    Bug: T108513
    Change-Id: I7e5a4bedc30e3bfa32995ad38eaef729cca9c362

--- modules/ext.dismissableSiteNotice.js
+++ modules/ext.dismissableSiteNotice.js
@@ -3,15 +3,20 @@
 	var cookieName = 'dismissSiteNotice',
 		siteNoticeId = mw.config.get( 'wgSiteNoticeId' );
 
+	// If no siteNoticeId is set, exit.
 	if ( !siteNoticeId ) {
 		return;
 	}
 
+	// If the user has the notice dismissal cookie set, exit.
 	if ( $.cookie( cookieName ) === siteNoticeId ) {
-		mw.util.addCSS( '.mw-dismissable-notice { display: none; }' );
 		return;
 	}
 
+	// Otherwise, show the notice ...
+	mw.util.addCSS( '.client-js .mw-dismissable-notice { display: block; }' );
+
+	// ... and enable the dismiss button.
 	$( function () {
 		$( '.mw-dismissable-notice-close' )
 			.css( 'visibility', 'visible' )
