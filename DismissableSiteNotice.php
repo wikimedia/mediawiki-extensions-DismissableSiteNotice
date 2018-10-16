9399c3855e82de13b72cfb4caab933861915a46c
Kunal Mehta <legoktm@member.fsf.org>
2018-10-16 11:28:35 -0700

    Warn if legacy PHP entry point is used
    Bug: T140851
    Change-Id: Iaa4c19898cf5c7b295c008bfc659c610a2b00c52

--- DismissableSiteNotice.php
+++ DismissableSiteNotice.php
@@ -18,11 +18,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'DismissableSiteNotice' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['DismissableSiteNotice'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for DismissableSiteNotice extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the DismissableSiteNotice extension requires MediaWiki 1.25+' );
